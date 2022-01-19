a882ce10dcd7a8d45b2b64ef135b589e40833cfe
chloefont <cfontaine@citycable.ch>
2022-01-19 11:44:28 +0100

    musician udp socket

--- src/musician/index.js
+++ src/musician/index.js
@@ -1,5 +1,12 @@
 const instruments = require("../shared/instruments");
 const protocol = require("../shared/protocol");
 const { v4: uuidv4 } = require('uuid');
+const dgram = require('dgram');
+
 let id = uuidv4();
-console.log(protocol);
+let socket = dgram.createSocket('udp4');
+
+socket.send(id, 0, id.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, function(err, bytes) {
+    console.log("Sending payload: " + id + " via port " + socket.address().port);
+});
+
