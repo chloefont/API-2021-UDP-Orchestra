9fbe00c1585518c3a502f7f6c1c1180737ef1864
chloefont <cfontaine@citycable.ch>
2022-01-19 11:22:27 +0100

    added uuid + instruments.js

--- src/musician/index.js
+++ src/musician/index.js
@@ -1,1 +1,4 @@
-console.log("hehe");
+const instruments = require("../shared/instruments")
+const { v4: uuidv4 } = require('uuid');
+let id = uuidv4();
+console.log(id);
---
+++ src/shared/instruments.js
@@ -0,0 +1,9 @@
+const instruments = {
+    piano: "ti-ta-ti",
+    trumpet: "pouet",
+    flute:"trulu",
+    violin: "gzi-gzi",
+    drum: "boum-boum"
+}
+
+module.exports = instruments;
