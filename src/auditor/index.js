a73bac1ae28c508dc0c1be7ae61305b4d54a3f05
Luca Coduri <coduri.vitor@gmail.com>
2022-01-19 11:35:23 +0100

    listening for message

--- src/auditor/index.js
+++ src/auditor/index.js
@@ -1,1 +1,13 @@
-console.log("coucou");
+const protocol = require("../shared/protocol");
+const dgram = require("dgram");
+
+const s = dgram.createSocket("udp4");
+
+s.bind(protocol.PROTOCOL_PORT, function () {
+    console.log("Joining multicast group");
+    s.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
+});
+
+s.on("message", function (msg, source) {
+    console.log("Data has arrived: " + msg + ". Source port: " + source.port);
+});
