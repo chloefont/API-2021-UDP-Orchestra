efff7c99f3d8bed62e5bde5351652410674f91d9
chloefont <cfontaine@citycable.ch>
2022-01-19 11:28:18 +0100

    added protocol

--- src/musician/index.js
+++ src/musician/index.js
@@ -1,4 +1,5 @@
-const instruments = require("../shared/instruments")
+const instruments = require("../shared/instruments");
+const protocol = require("../shared/protocol");
 const { v4: uuidv4 } = require('uuid');
 let id = uuidv4();
-console.log(id);
+console.log(protocol);
