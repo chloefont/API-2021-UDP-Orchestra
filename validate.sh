11a7a1828372c68131be29aa55120dd3a163d572
Luca <coduri.vitor@gmail.com>
2022-01-29 19:52:36 +0100

    modification script

--- validate.sh
+++ validate.sh
@@ -29,8 +29,8 @@ echo ""
 echo ""
 echo "*** Rebuilding our 3 Docker images"
 echo ""
-docker build --tag api/musician --file ./docker/image-musician/Dockerfile ./docker/image-musician/
-docker build --tag api/auditor --file ./docker/image-auditor/Dockerfile ./docker/image-auditor/
+docker build --tag api/musician --file ./docker/image-musician/Dockerfile .
+docker build --tag api/auditor --file ./docker/image-auditor/Dockerfile .
 docker build --tag api/validate-music --file ./docker/image-validation/Dockerfile ./docker/image-validation/
 
 #
